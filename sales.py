7f0754ea2942cd1591622912fd34e94e731ba798
Ellcyo Castro <aellcyo@gmail.com>
2020-07-10 10:18:29 -0400

    Status

--- sales.py
+++ sales.py
@@ -45,6 +45,10 @@ def product_corrs():
     return series.to_json()
 
 
+@app.route('/v1/status', methods=['GET'])
+def status():
+    return "status ok..."
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
